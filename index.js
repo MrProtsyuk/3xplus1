48d9cdfaa46a160be9c55a9c9f67672e45b9520d
Mark Protsyuk <marcprotsyuk@gmail.com>
2024-05-03 21:47:37 -0700

    couldn't solve the issues with the recursion

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ let user_message = document.getElementById("user_message");
 
 function even_check(user_input) {
   // checks if the number is 1; if one then it breaks the 4,2,1 loop and ends the conditional loop
+  if (user_input % 2 == 0) {
+    user_input = user_input / 2;
+    num_list.push(user_input);
+    even_check();
+  }
+
+  // checks if the number is even; if even then it is divided by 2 and thrown back into the conditional
   if (user_input == 1) {
     num_list.push(user_input);
     document.getElementById("num_array").innerHTML = num_list;
     document.getElementById("user_message").innerHTML =
       "Unfortunalty you hit 1! That means that the formula will continue to spit out 4, 2, 1 forever. For the Purposes of this program I made sure to cut it off at 1 ;)";
-  }
-
-  // checks if the number is even; if even then it is divided by 2 and thrown back into the conditional
-  else if (user_input % 2 == 0) {
-    user_input = user_input / 2;
-    num_list.push(user_input);
-    even_check(user_input);
   }
   // checks if there are any repeated numbers; repeated numbers create loops outside the 4,2,1 loop
   //   else if ((checkForDuplicates = true)) {
@@ -30,9 +30,10 @@ function even_check(user_input) {
   //       "Hey! You have found a loop outside of the 4,2,1 loop! Congratulations, you might possibly be the first person in history to do so.";
   //   }
   // checks if the number is odd; if odd it *3 and adds one then is thrown back into the conditional
-  else if (user_input % 2 != 0) {
+  if (user_input % 2 != 0 && user_input != 1) {
     user_input = user_input * 3 + 1;
     num_list.push(user_input);
-    even_check(user_input);
   }
 }
+
+even_check(user_input);
